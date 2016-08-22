d6f7050a3d8cd9ac29d3913f06d78335be3558e8
remy <remy.auricoste@gmail.com>
2016-08-22 17:04:58 +0200

    fix mkdir dist dir + deps

--- src/build.js
+++ src/build.js
@@ -3,13 +3,20 @@
 var BuilderBrowserify = require("./BuilderBrowserify");
 var BuilderAssets = require("./BuilderAssets");
 var BuilderCopy = require("./BuilderCopy");
+var File = require("rauricoste-file");
+var Q = require("rauricoste-promise-light");
 
 module.exports = function(config) {
     var configHelper = require("./ConfigHelper")(config);
     var modules = configHelper.getModules();
 
-    modules.forEach(BuilderBrowserify);
-    ["css", "html"].forEach(function(extension) {
-        modules.forEach(BuilderAssets(extension));
+    Q.all(modules.map(function(module) {
+        console.log("mkdir", module.dist);
+        return new File(module.dist).mkdir().catch(function() {});
+    })).then(function() {
+        modules.forEach(BuilderBrowserify);
+        ["css", "html"].forEach(function(extension) {
+            modules.forEach(BuilderAssets(extension));
+        })
     })
 }
