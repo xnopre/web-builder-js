410b42032cf3429943703666b89b2832fa3b7db7
remy <remy.auricoste@gmail.com>
2017-04-30 10:11:45 +0200

    fix tests

--- src/TemplateTranspiler.spec.js
+++ src/TemplateTranspiler.spec.js
@@ -3,21 +3,23 @@ var TemplateTranspiler = require("./TemplateTranspiler");
 describe("TemplateTranspiler", () => {
     describe("interpret method", () => {
         it("should transpile @{} into ${includeTemplate()}", () => {
-            expect(TemplateTranspiler.interpret(`
-                <div>${title}</div>
+            expect(TemplateTranspiler.interpret(
+                `<div>\${title}</div>
                 @{"plop.html", {
                     array: [],
                     subObject: {}
                 }}
-                <div>${anotherTitle}</div>
-            `)).to.equal(`
-                <div>${title}</div>
-                ${includeTemplate("plop.html", {
+                <div>\${anotherTitle}</div>`
+            )).to.deep.equal({
+                deps: ["plop.html"],
+                content:
+                `<div>\${title}</div>
+                \${includeTemplate("plop.html", {
                     array: [],
                     subObject: {}
                 })}
-                <div>${anotherTitle}</div>
-            `)
+                <div>\${anotherTitle}</div>`
+            })
         })
     })
 })
